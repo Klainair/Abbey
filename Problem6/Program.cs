305ff5ea1eb87775793b43e2fa31aacbde6ff52c
klainair <Klainair@yandex.ru>
2016-12-06 10:34:10 +0300

    Complete

--- Problem6/Program.cs
+++ Problem6/Program.cs
@@ -6,7 +6,21 @@ namespace Problem6
 	{
 		public static void Main (string[] args)
 		{
-			
+			string[] str = Console.ReadLine ().Split (' ');
+			int max = Convert.ToInt32 (str [0]);
+			int min = Convert.ToInt32 (str [0]);
+
+			for (int i = 0; i < str.Length; i++) {
+				if (Convert.ToInt32 (str [i]) > max) {
+					max = str [i];
+				}
+
+				if (Convert.ToInt32 (str [i]) < min) {
+					min = str [i];
+				}
+			}
+
+			Console.Write ("{0} {1}", max, min);
 		}
 	}
 }
